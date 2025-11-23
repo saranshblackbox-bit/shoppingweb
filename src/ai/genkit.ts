770ed2c0f6b345289392af8ac1793c888ea4df5a
Saransh <saranshgemini18@gmail.com>
2025-11-23 12:21:49 +0000

    ai not working

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,8 @@
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import {nextHandler} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(), nextHandler()],
   enableTracingAndMetrics: true,
 });
