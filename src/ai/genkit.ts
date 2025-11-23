abd68f2c57b2376bd199e9d1e5aa35dba57538c7
Saransh <saranshgemini18@gmail.com>
2025-11-23 11:53:59 +0000

    same error coming i think i have to shift other ai like chatgptr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,8 +3,6 @@ import {googleAI} from '@genkit-ai/google-genai';
 import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI(), nextPlugin({
-    api: '/api/genkit',
-  })],
+  plugins: [googleAI(), nextPlugin()],
   model: 'googleai/gemini-2.5-flash',
 });
